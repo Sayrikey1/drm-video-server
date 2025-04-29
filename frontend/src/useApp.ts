31c715a882bf32ff0dcd3195c3420bc81facbeea
Kamal <kamalseriki49@gmail.com>
2025-04-30 00:01:23 +0100

    final

--- frontend/src/useApp.ts
+++ frontend/src/useApp.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { VideoInfo } from "./VideoPlayer/useVideoPlayer";
 
-const SAMPLE_S3_FILE_KEY = "file1/result/hls/sample_video_1080p.m3u8";
+// const SAMPLE_S3_FILE_KEY = "file1/result/hls/sample_video_1080p.m3u8";
+const SAMPLE_S3_FILE_KEY = "59641f67-b958-465d-aa08-95ec6412338c";
 const HLS_MIME_TYPE = "application/x-mpegURL"; // mime-type for HLS protocol
 
 interface AbsContentResult {
@@ -35,7 +36,7 @@ const useApp = () => {
     const mimeType = HLS_MIME_TYPE;
 
     const result = await axios.get(
-      `https://abs-data.mpcsj.online/abs/${s3FileKey}/access`,
+      `https://api.streaming.mabi-vids.com/abs/${s3FileKey}/access`,
       {
         withCredentials: true, // to set cookies from backend to frontend
       }
